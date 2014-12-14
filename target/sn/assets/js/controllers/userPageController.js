c57601568d85622231f64ed19418b34e90922e42
Oleh Chaplya <olehch7@gmail.com>
2014-12-14 03:08:41 +0200

    Basic git api

---
+++ target/sn/assets/js/controllers/userPageController.js
@@ -0,0 +1,9 @@
+/**
+* Created by Oleh on 13.12.2014.
+*/
+
+var phonecatApp = angular.module('userPage', []);
+
+phonecatApp.controller('userInfo', function ($scope) {
+    $scope.test = "testisfefefedewdewdng";
+});
